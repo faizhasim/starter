4ecc2cf71ecebb9f7ddf75ffacbc2fbfd4b8fdf0
Mohd Faiz Hasim <faizhasim@gmail.com>
2022-04-09 09:21:29 +0800

    chore: Add hack for `.m2/settings-security.xml`

--- installers/mackup.sh
+++ installers/mackup.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
-
 cat <<EOF > ~/.mackup.cfg
 [storage]
 engine = icloud
+EOF
+
+mkdir -p ~/.mackup
+touch ~/.mackup/maven-settings-security-hack.cfg
+
+cat <<EOF > ~/.mackup/maven-settings-security-hack.cfg
+[application]
+name = Maven settings-security.xml hack
+
+[configuration_files]
+.m2/settings-security.xml
 EOF
