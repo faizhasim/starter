66e37a471e010f41361dcad8112cc12a1e76135f
Mohd Faiz Hasim <faizhasim@gmail.com>
2022-07-09 20:53:00 +0800

    Install node via `n`

--- installers/node.sh
+++ installers/node.sh
@@ -5,7 +5,18 @@
 ###############################################################################
 
 install_node() {
+  # make cache folder (if missing) and take ownership
+  sudo mkdir -p /usr/local/n
+  sudo chown -R $(whoami) /usr/local/n
+  # make sure the required folders exist (safe to execute even if they already exist)
+  sudo mkdir -p /usr/local/bin /usr/local/lib /usr/local/include /usr/local/share
+  # take ownership of Node.js install destination folders
+  sudo chown -R $(whoami) /usr/local/bin /usr/local/lib /usr/local/include /usr/local/share
   n lts
+  curl -L https://raw.githubusercontent.com/tj/n/master/bin/n -o n
+  bash n lts
+  # Now node and npm are available
+  npm install -g n
 }
 
 # Node and yarn *should* be installed at this point
